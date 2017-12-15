99f56f091bb6567eab912722a4d3df4ea3d03003
Danielle H Gamble <dg12181@srcs.loc>
2017-12-15 10:44:36 -0800

    completed part one besides decimals and negatives

--- Final_Challenge_functions.js
+++ Final_Challenge_functions.js
@@ -1,6 +1,6 @@
 function checkPrime(number) {
-    for(i = 2; i <= number/2; i++;){
-        if(number%i == 0){
+    for(i = 2; i <= (number)/2; i++){
+        if ((number)%i == 0 || (number < 1)){
             return "not prime";
         }
     }
